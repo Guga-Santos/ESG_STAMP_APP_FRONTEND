c49181ab0c3317bfb40cb8b0132098f27454a56d
GugaSantos <guga.santos.guga@gmail.com>
2023-09-12 16:24:32 -0300

    feat: gh-pages

--- src/app/components/card.tsx
+++ src/app/components/card.tsx
@@ -21,7 +21,7 @@ export default function Card({props}: {props: ResponseAPI}) {
         focus:ring-4 focus:outline-none focus:ring-blue-300 
         font-medium rounded-lg text-sm px-5 py-2.5 text-center 
         inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 
-        dark:focus:ring-blue-800 mt-4"
+        dark:focus:ring-blue-800 mt-4 w-full"
         onClick={() => handleClick()}>
         Acesse o relatório:
           <svg className="w-3.5 h-3.5 ml-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
